6f836ad6a6cdc843fad6b009bfd280bc50a263ae
Elena Pavlova <lenlho22@gmail.com>
2024-10-06 23:39:37 +0300

    feat: implement write function with stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import { createWriteStream } from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
 const write = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const fileWritePath = `${__dirname}/files/fileToWrite.txt`;
+
+  const stream = createWriteStream(fileWritePath);
+  process.stdin.pipe(stream);
+
+  stream.on("finish", () => {
+    console.log("File written");
+  });
+
+  stream.on("error", () => {
+    console.error("FS operation failed");
+  });
 };
 
 await write();
