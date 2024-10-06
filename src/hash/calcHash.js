2f096e844229740cf85c59427c557d02f52e31b7
Elena Pavlova <lenlho22@gmail.com>
2024-10-06 23:06:05 +0300

    feat: implement calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = `${__dirname}/files/fileToCalculateHashFor.txt`;
+
+  const stream = createReadStream(filePath);
+  const hash = createHash("sha256");
+
+  stream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+
+  stream.on("end", () => {
+    const hexHash = hash.digest("hex");
+    console.log(`SHA256 hash: ${hexHash}`);
+  });
+
+  stream.on("error", () => {
+    console.error("FS operation failed");
+  });
 };
 
 await calculateHash();
