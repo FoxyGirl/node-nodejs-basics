ba3a2a30c3252d68efa36af795bd8ffb69d42e9e
Elena Pavlova <lenlho22@gmail.com>
2024-10-06 23:27:53 +0300

    feat: implement read function with stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream } from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = `${__dirname}/files/fileToRead.txt`;
+
+  try {
+    const readStream = createReadStream(filePath);
+    readStream.pipe(process.stdout);
+  } catch {
+    console.error("FS operation failed");
+  }
 };
 
 await read();
