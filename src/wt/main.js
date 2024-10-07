7491ef8a5e933a8510b8921ddf4aebd33be79aa7
Elena Pavlova <lenlho22@gmail.com>
2024-10-07 21:15:42 +0300

    feat: implement workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import { Worker, parentPort } from "worker_threads";
+import { fileURLToPath } from "url";
+import path from "path";
+import { cpus } from "os";
+
 const performCalculations = async () => {
-    // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const workerFilePath = `${__dirname}/worker.js`;
+
+  const workersNumber = cpus().length;
+  const initialN = 10;
+
+  const promises = [...new Array(workersNumber)].map(
+    (_, index) =>
+      new Promise((resolve, reject) => {
+        const worker = new Worker(workerFilePath, {
+          workerData: { n: initialN + index },
+        });
+
+        worker.on("message", ({ result }) => resolve(result));
+
+        worker.on("error", reject);
+      })
+  );
+
+  const allResults = await Promise.allSettled(promises);
+  const result = allResults.map((item) =>
+    item.status === "fulfilled"
+      ? { status: "resolved", data: item.value }
+      : { status: "error", data: null }
+  );
+
+  console.log(result);
 };
 
 await performCalculations();
